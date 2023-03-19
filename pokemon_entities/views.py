362e31c2ff333322ea2aa1640fc61f2e4f107d36
Roman Okatev <43115066+Nebulawalker@users.noreply.github.com>
2023-03-19 23:10:14 +0300

    added Japanese and English titles of pokemons

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -78,7 +78,9 @@ def show_pokemon(request, pokemon_id):
     pokemon = {
         'title_ru': requested_pokemon.title,
         'img_url': image_url,
-        'description': requested_pokemon.description
+        'description': requested_pokemon.description,
+        'title_en': requested_pokemon.title_en,
+        'title_jp': requested_pokemon.title_jp
     }
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
