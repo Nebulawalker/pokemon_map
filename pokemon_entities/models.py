055e65e80786a004bab7836f1a9e4991c65eac1a
Roman Okatev <43115066+Nebulawalker@users.noreply.github.com>
2023-03-22 22:34:06 +0300

    add related_name to PokemonEntity model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -44,7 +44,8 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(
         Pokemon,
         on_delete=models.CASCADE,
-        verbose_name='Покемон'
+        verbose_name='Покемон',
+        related_name='entities'
     )
     lat = models.FloatField(verbose_name='Широта')
     lon = models.FloatField(verbose_name='Долгота')
