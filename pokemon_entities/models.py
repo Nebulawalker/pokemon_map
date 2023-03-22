803a1fb769cc07a5b85b0156bbf7efb122587b27
Roman Okatev <43115066+Nebulawalker@users.noreply.github.com>
2023-03-22 22:50:03 +0300

    Change ForeignKey on_delete for PokemonEntity model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -43,7 +43,7 @@ class Pokemon(models.Model):
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(
         Pokemon,
-        on_delete=models.CASCADE,
+        on_delete=models.PROTECT,
         verbose_name='Покемон',
         related_name='entities'
     )
